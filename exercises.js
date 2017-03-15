8667350ff82c7ce83dd2b524105926960df47721
Cailyn <cailyn.s.e@gmail.com>
2017-03-15 13:00:35 -0400

    Finished

--- exercises.js
+++ exercises.js
@@ -111,17 +111,30 @@ function incrementByOne(arr) {
 	//arr is an array of integers  
 	//increase each integer by one
 	//return the array
+	var newArr = [],
+		newInt;
+	for (var i = 0; i < arr.length; i++) {
+		newInt = arr[i] + 1;
+		newArr.push(newInt);
+	}
+	return newArr;
 }
 
 function addItemToArray(arr, item) {
 	//add the item to the end of the array
 	//return the array
+	var newArr = arr;
+	newArr.push(item);
+	return newArr;
 }
 
 function addItemToFront(arr, item) {
 	//add the item to the front of the array
 	//return the array
 	//hint: use the array method .unshift
+	var newArr = arr;
+	newArr.unshift(item);
+	return newArr;
 }
 
 function wordsToSentence(words) {
@@ -129,26 +142,54 @@ function wordsToSentence(words) {
 	//return a string that is all of the words concatenated together
 	//spaces need to be between each word
 	//example: ['Hello', 'world!'] -> 'Hello world!'
+	var newString = words.join(' ');
+	return newString;
 }
 
 function contains(arr, item) {
 	//check to see if item is inside of arr
 	//return true if it is, otherwise return false
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] === item) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function addNumbers(numbers) {
 	//numbers is an array of integers.
 	//add all of the integers and return the value
+	var sum = numbers[0];
+	for (var i = 1; i < numbers.length; i++) {
+		sum = sum + numbers[i];
+	}
+	return sum;
 }
 
 function averageTestScore(testScores) {
 	//testScores is an array.  Iterate over testScores and compute the average.
 	//return the average
+	var numberOfScores = testScores.length,
+		sumOfScores = testScores[0],
+		averageScore;
+	for (var i = 1; i < testScores.length; i++) {
+		sumOfScores = sumOfScores + testScores[i];
+	}
+	averageScore = sumOfScores / numberOfScores;
+	return averageScore;
 }
 
 function largestNumber(numbers) {
 	//numbers is an array of integers
 	//return the largest integer
+	var largest = numbers[0];
+	for (var i = 1; i < numbers.length; i++) {
+		if (numbers[i] > largest) {
+			largest = numbers[i];
+		}
+	}
+	return largest;
 }
 
 //Do not modify code below this line.
